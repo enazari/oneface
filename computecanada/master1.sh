bc15f63cfb4c0452d6cff705b8d0fba9c5bd6c0a
enazari <csnazari@gmail.com>
2023-12-28 01:39:44 -0500

    Modified the timing for the testing sh file

--- computecanada/master1.sh
+++ computecanada/master1.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=7G            # memory for the entire job across all cores
-#SBATCH --time=00-11:00      # time (DD-HH:MM)
+#SBATCH --time=00-04:00      # time (DD-HH:MM)
 #SBATCH --output=out_%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=csnazari@gmail.com   # Email to which notifications will be $
